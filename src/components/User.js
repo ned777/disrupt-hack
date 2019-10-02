2f143868cef0c27a48b6f03fd3161bf57ff91b73
Walter Tlatenchi <walter.tlatenchi@gmail.com>
2019-10-02 12:30:37 -0700

    set-up router

---
+++ src/components/User.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class User extends Component {
+  render() {
+    return (
+      <div>
+        This is the user page
+      </div>
+    );
+  }
+}
+
+export default User;
